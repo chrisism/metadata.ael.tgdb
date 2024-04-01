cefa3280c0e4d1c7ee4035fe04a5cbd951ba846c
chrisism <crizizz@gmail.com>
2024-04-01 16:44:23 +0200

    Fix arg to int

--- default.py
+++ default.py
@@ -56,7 +56,7 @@ def run_plugin():
     parser.add_argument('--romcollection_id', type=str, help="ROM Collection ID")
     parser.add_argument('--source_id', type=str, help="Source ID")
     parser.add_argument('--entity_id', type=str, help="Entity ID")
-    parser.add_argument('--entity_type', type=str, help="Entity Type (ROM|ROMCOLLECTION|SOURCE)")
+    parser.add_argument('--entity_type', type=int, help="Entity Type (ROM|ROMCOLLECTION|SOURCE)")
     parser.add_argument('--akl_addon_id', type=str, help="Addon configuration ID")
     parser.add_argument('--settings', type=json.loads, help="Specific run setting")
     
@@ -91,14 +91,16 @@ def run_scraper(args):
         settings,
         TheGamesDB(),
         pdialog)
-                    
+    
     if args.entity_type == constants.OBJ_ROM:
+        logger.debug("Single ROM processing")
         scraped_rom = scraper_strategy.process_single_rom(args.entity_id)
         pdialog.endProgress()
         pdialog.startProgress('Saving ROM in database ...')
         scraper_strategy.store_scraped_rom(args.akl_addon_id, args.entity_id, scraped_rom)
         pdialog.endProgress()
     else:
+        logger.debug("Multiple ROM processing")
         scraped_roms = scraper_strategy.process_roms(args.entity_type, args.entity_id)
         pdialog.endProgress()
         pdialog.startProgress('Saving ROMs in database ...')
